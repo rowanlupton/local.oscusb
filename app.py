1ac80d38abe116fb38e8988c83a2cc315cfd0a25
Rowan Lupton <rowan@livingtradition.tech>
2025-09-30 17:39:52 -0400

    json load/write of settings works - i think this marks minimum viable

--- app.py
+++ app.py
@@ -13,14 +13,24 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def settings():
-    with open('settings.json', 'r') as f:
-        settings = json.load(f)
-    f.close()
-    
     if request.method == 'POST':
-        print(request.form)
+        with open('settings.json', 'r') as f:
+            settings = json.load(f)
+        with open('settings.json', 'w') as f:
+            #print('old settings: ',settings)
+            formAttrs = request.form
+            for attr in request.form:
+                settings[attr]['value'] = formAttrs[attr]
+            
+            print('new settings: ',settings)
+            json.dump(settings, f, indent=4)
+            #f.close()
+        
         return redirect('/')
-        #return request.form
     else:
+        with open('settings.json', 'r') as f:
+            settings = json.load(f)
+        f.close()
         return render_template('settings.html', settings=settings)
+        return 'hi'
 
